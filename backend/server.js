dc0a3810a172275d1fdb8168f0164b8b23833f2c
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-09-03 17:27:57 +0000

    MongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db')
+
+connectDB()
 Port = process.env.PORT ||  5000
 
 const app = express()
