67bd204bdf1b4ccf046345e0e8f2d77d5a50ee88
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-09-03 17:45:37 +0000

    Error midlleware created

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db')
+const { errorHandler } = require('./middlewares/errorMiddleware')
 
 connectDB()
 Port = process.env.PORT ||  5000
@@ -11,5 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
 app.use('/api/goals' ,require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(Port, ()=> console.log(`Server is running  port ${Port}`))
