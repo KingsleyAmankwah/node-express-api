714115b891e21cb230fd5bb464f3e88cb5d6147c
Kingsley Amankwah <kuamiklassic149@gmail.com>
2022-11-23 01:45:28 +0000

    Server js

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
-const express = require('express')
-const dotenv = require('dotenv').config()
-const connectDB = require('./config/db')
-const { errorHandler } = require('./middlewares/errorMiddleware')
+const express = require("express");
+const dotenv = require("dotenv").config();
+const connectDB = require("./config/db");
+const { errorHandler } = require("./middlewares/errorMiddleware");
 
-connectDB()
-Port = process.env.PORT ||  5000
+connectDB();
+Port = process.env.PORT || 5000;
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.use('/api/goals' ,require('./routes/goalRoutes'))
+app.use("/api/goals", require("./routes/goalRoutes"));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(Port, ()=> console.log(`Server is running  port ${Port}`))
+app.listen(Port, () => console.log(`Server is running  port ${Port}`));
